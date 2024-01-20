9016686c28e2bb25bf40b134d106ca33003b3c4e
rive <rive@MSI>
2024-01-20 10:56:34 -0600

    Rpimer Commit

---
+++ 012024/src/main/java/seleniumPractica/PracticaSel.java
@@ -0,0 +1,14 @@
+package seleniumPractica;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class PracticaSel {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
